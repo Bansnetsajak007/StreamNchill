5ac35707bdb52c2e81336ab1788add478e0ac521
sajak <sajakbasnet77@gmail.com>
2024-10-04 21:46:41 +0545

    Fix: Cors related issue

--- server/index.js
+++ server/index.js
@@ -7,13 +7,13 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://streamnchill.vercel.app',
     methods: ['GET', 'POST'],
   },
 });
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://streamnchill.vercel.app',
 }));
 
 const PORT = process.env.PORT || 3001;
